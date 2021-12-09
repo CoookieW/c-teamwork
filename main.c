701688d1ad6f9dc0726fa834ca2c3965b4ecaa71
CoookieW <95843109+CoookieW@users.noreply.github.com>
2021-12-09 15:54:50 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char CustomName;
+
+
+
+    printf(" ");
+    scanf("%c",CustomName);
+    printf(" ");
+    scanf("%c",Number);
+    printf(" ");
+    printf("ABCD")
+    scanf("%c",choice);
+
+    if(choice=A)
+    {
+        //点餐
+        {
+            //奶茶
+            {
+                //名称
+                {
+                    //小料 甜度选择
+                }
+            }
+            //甜点
+            //冰激凌
+        }
+
+    }
+    if(B)
+    {
+        //shoppingbag
+    }
+    if(C)
+    {
+        //
+    }
+
+}
