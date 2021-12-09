a4b096c6193d1e632fd7da280f169627abfd54df
CoookieW <95843109+CoookieW@users.noreply.github.com>
2021-12-09 16:16:27 +0800

    Add files via upload

---
+++ 客人信息.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct custominformation;
+    {
+        char customname;
+        long int telephone;
+        char address;
+
+    };
+    typedef struct custominformation CI;
+    return 0;
+}
