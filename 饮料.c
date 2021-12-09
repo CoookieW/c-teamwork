e4a235ac2490205c139c55a4e1317f8d0445e2b0
CoookieW <95843109+CoookieW@users.noreply.github.com>
2021-12-09 16:19:12 +0800

    Add files via upload

---
+++ 饮料.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct drinks;
+    {
+        char drinkname;
+        int drinkprice;
+
+    };
+    typedef struct drinks DS;
+    return 0;
+}
